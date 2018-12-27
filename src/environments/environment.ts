3c28bb605ce78f30747a16f523fbfef08068d0e6
ReneMurillo <rene.murillo95@hotmail.com>
2018-12-27 10:45:09 -0600

    Se agregaron los componentes list y form

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase : {
+    apiKey: "AIzaSyBAzRp3vLILdkYWljTVsM4vTrd11gpkqXk",
+    authDomain: "todo-app-19564.firebaseapp.com",
+    databaseURL: "https://todo-app-19564.firebaseio.com",
+    projectId: "todo-app-19564",
+    storageBucket: "todo-app-19564.appspot.com",
+    messagingSenderId: "221190875622"
+  }
 };
 
 /*
